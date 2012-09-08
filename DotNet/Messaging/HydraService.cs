bce8dbe14dfc0f14ce6db72fb52e51dddfd93861
NickNorth <North.N@gmail.com>
2012-09-08 16:57:54 +0100

    DefaultListenerOptions property to allow dynamic modification.

--- DotNet/Messaging/HydraService.cs
+++ DotNet/Messaging/HydraService.cs
@@ -9,19 +9,20 @@ namespace Bollywell.Hydra.Messaging
     public class HydraService : IHydraService
     {
         private readonly IProvider _provider;
-        private readonly ListenerOptions _listenerOptions;
 
-        public HydraService(IProvider provider, ListenerOptions listenerOptions = null)
+        public ListenerOptions DefaultListenerOptions { get; set; }
+
+        public HydraService(IProvider provider, ListenerOptions defaultListenerOptions = null)
         {
             _provider = provider;
-            _listenerOptions = listenerOptions;
+            DefaultListenerOptions = defaultListenerOptions;
         }
 
         #region Implementation of IHydraService
 
         public IListener<TMessage> GetListener<TMessage>(IMessageFetcher<TMessage> messageFetcher, IMessageId startId = null, ListenerOptions listenerOptions = null) where TMessage : TransportMessage
         {
-            return new Listener<TMessage>(_provider, messageFetcher, startId, listenerOptions ?? _listenerOptions);
+            return new Listener<TMessage>(_provider, messageFetcher, startId, listenerOptions ?? DefaultListenerOptions);
         }
 
         public IMessageId Send<TMessage>(TMessage message) where TMessage : TransportMessage
