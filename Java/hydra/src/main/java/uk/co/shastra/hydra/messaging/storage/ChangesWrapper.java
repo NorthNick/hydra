8d17d2cdd18ffc065e1596443ea51604c0f360f2
NickNorth <North.N@gmail.com>
2013-07-27 14:11:19 +0100

    POM improvements

--- Java/hydra/src/main/java/uk/co/shastra/hydra/messaging/storage/ChangesWrapper.java
+++ Java/hydra/src/main/java/uk/co/shastra/hydra/messaging/storage/ChangesWrapper.java
@@ -5,7 +5,7 @@ import uk.co.shastra.hydra.messaging.messageids.MessageId;
 public class ChangesWrapper {
 	public Iterable<MessageId> messageIds;
 	public long lastSeq;
-	
+
 	public ChangesWrapper(Iterable<MessageId> messageIds, long lastSeq) {
 		super();
 		this.messageIds = messageIds;
