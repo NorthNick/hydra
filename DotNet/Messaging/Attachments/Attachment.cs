368c7a284c6aad9b96d3f2c860a38945f85dc167
NickNorth <North.N@gmail.com>
2013-11-02 15:32:36 +0000

    Missed files

---
+++ DotNet/Messaging/Attachments/Attachment.cs
@@ -0,0 +1,20 @@
+﻿
+using Shastra.Hydra.Messaging.MessageIds;
+
+namespace Shastra.Hydra.Messaging.Attachments
+{
+    public class Attachment
+    {
+        public string ContentType { get; private set; }
+        public IMessageId MessageId { get; set; }
+        public string Name { get; private set; }
+
+        // This base type is only for use internally, hence the constructor restriction
+        internal Attachment(string name, string contentType, IMessageId messageId = null)
+        {
+            Name = name;
+            ContentType = contentType;
+            MessageId = messageId;
+        }
+    }
+}
---
+++ DotNet/Messaging/Attachments/StringAttachment.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shastra.Hydra.Messaging.Attachments
+{
+    public class StringAttachment : Attachment
+    {
+        private const string TextContentType = "text/plain";
+
+        public string Data { get; private set; }
+
+        public StringAttachment(string name, string data) : base(name, TextContentType)
+        {
+            Data = data;
+        }
+    }
+}
